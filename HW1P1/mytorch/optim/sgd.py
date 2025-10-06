bf539d95089b51ace899c60be352d941452ee37f
Dang Truong <truongminhdang26403@gmail.com>
2025-10-06 19:31:50 -0400

    HW1P1: finish implementing SGD with momentum

--- HW1P1/mytorch/optim/sgd.py
+++ HW1P1/mytorch/optim/sgd.py
@@ -2,30 +2,25 @@ import numpy as np
 
 
 class SGD:
-
     def __init__(self, model, lr=0.1, momentum=0):
-
         self.l = model.layers
         self.L = len(model.layers)
         self.lr = lr
         self.mu = momentum
-        self.v_W = [np.zeros(self.l[i].W.shape, dtype="f")
-                    for i in range(self.L)]
-        self.v_b = [np.zeros(self.l[i].b.shape, dtype="f")
-                    for i in range(self.L)]
+        if momentum > 0:
+            self.v_W = [np.zeros(self.l[i].W.shape, dtype="f") for i in range(self.L)]
+            self.v_b = [np.zeros(self.l[i].b.shape, dtype="f") for i in range(self.L)]
+        else:
+            self.v_W = None
+            self.v_b = None
 
     def step(self):
-
         for i in range(self.L):
-
             if self.mu == 0:
-
-                self.l[i].W = None  # TODO
-                self.l[i].b = None  # TODO
-
+                self.l[i].W = self.l[i].W - self.lr * self.l[i].dLdW  # TODO
+                self.l[i].b = self.l[i].b - self.lr * self.l[i].dLdb  # TODO
             else:
-
-                self.v_W[i] = None  # TODO
-                self.v_b[i] = None  # TODO
-                self.l[i].W = None  # TODO
-                self.l[i].b = None  # TODO
+                self.v_W[i] = self.mu * self.v_W[i] + self.lr * self.l[i].dLdW  # TODO
+                self.v_b[i] = self.mu * self.v_b[i] + self.lr * self.l[i].dLdb  # TODO
+                self.l[i].W = self.l[i].W - self.v_W[i]  # TODO
+                self.l[i].b = self.l[i].b - self.v_b[i]  # TODO
